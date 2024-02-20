0fcf03e8b1633b801233ed29f97bce4aac563e42
Vris111 <ruslansimanovic56@mail.ru>
2024-02-20 18:07:05 +0700

    cart create

--- my-app/src/views/CartView.vue
+++ my-app/src/views/CartView.vue
@@ -1,11 +1,46 @@
-<script setup>
+<script>
+import store from "@/store";
+
+export default {
+  computed: {
+    store() {
+      return store
+    }
+  },
+  data(){
+    return{
+      countItemId: 0,
+    }
+  },
+}
 
 </script>
 
 <template>
-
+  <h2>Products in cart:</h2>
+  <div class="cart">
+    <div class="item" v-for="product in store.state.cart" :key="product.id">
+      <p>Название: {{ product.name }}</p>
+      <p>Описание: {{ product.description }}</p>
+      <p>Цена: {{ product.price }}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.item{
+  border: 1px solid black;
+  padding: 5px;
+  width: 220px;
+  height: 400px;
+  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+}
 
+.cart {
+  display: grid;
+  grid-template-columns: 150px 150px 150px 150px 150px;
+  gap: 90px;
+}
 </style>
