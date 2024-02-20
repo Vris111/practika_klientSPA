8b63e3ba4ff5b98dfd5eed0bfb6d72e739b76b8d
Vris111 <ruslansimanovic56@mail.ru>
2024-02-20 17:50:59 +0700

    connect to api for take data

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,11 +1,25 @@
 import { createStore } from 'vuex'
+import axios from "axios";
 
 export default createStore({
   state: {
+    products: [],
+    cart: [],
   },
   getters: {
   },
   mutations: {
+    addToCart(state, product){
+      state.cart.push(product);
+      console.log(state.cart);
+    },
+    async getProducts(state){
+      const {data} = await axios.get('https://jurapro.bhuser.ru/api-shop/products')
+          .then(response => state.products = response.data)
+          .catch(error =>{console.log(error)})
+      state.products = data;
+      console.log(state.products)
+    },
   },
   actions: {
   },
--- my-app/src/views/HomeView.vue
+++ my-app/src/views/HomeView.vue
@@ -1,11 +1,49 @@
-<script setup>
+<script>
+import store from "@/store";
+export default {
+  computed: {
+    store() {
+      return store
+    }
+  },
+  methods:{
 
+  },
+  mounted() {
+    this.$store.commit('getProducts');
+  }
+}
 </script>
 
 <template>
-
+  <div>
+    <ul class="products_all">
+      <p class="item" v-for="(product, index) in store.state.products" :key="product.id">
+        {{product.name}}
+        <p></p>
+        {{product.description}}
+        <p></p>
+        {{product.price}}
+        <button class="addItemInCart"  @click="store.commit('addToCart', product)">Buy</button>
+      </p>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
+.item{
+  border: 1px solid black;
+  padding: 5px;
+  width: 220px;
+  height: 400px;
+  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+}
 
+.products_all {
+  display: grid;
+  grid-template-columns: 150px 150px 150px 150px 150px;
+  gap: 90px;
+}
 </style>
